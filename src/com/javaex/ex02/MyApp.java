b6463c98469eae3842a7ab95d36c19940c8d4013
ahah2025 <adghty61>
2025-05-13 12:25:54 +0900

    2025-05-13

--- src/com/javaex/ex02/MyApp.java
+++ src/com/javaex/ex02/MyApp.java
@@ -4,7 +4,8 @@ public class MyApp {
 
 	public static void main(String[] args) {
 		//*포인트 포인트별로 관리, 원은 원별로 관리
-		//포인트(포인트만 관리)
+		
+		//포인트(포인트만 관리)/////////////////////////////
 		//포인트 리스트
 		MyList pList = new MyList();
 		
@@ -19,12 +20,34 @@ public class MyApp {
 		pList.add(p03);
 		
 		//출력
-		((Point)pList.get(0)).getX()
-		System.out.println(pList.get(0).toString());
-		System.out.println(pList.get(1).toString());
-		System.out.println(pList.get(2).toString());
+		for(int i=0; i<pList.size(); i++) {
+			System.out.println(pList.get(i).toString());
+		}
 		
+		//pList.get(0).getX();
+		//((Point)pList.get(0)).getX();  //다운캐스팅 해야된다
 		
+		System.out.println("===============================");
+		
+		//원(원만 관리)/////////////////////////////
+		//원리스트
+		MyList cList = new MyList(); //**MyList 하나로 여러번 쓸 수 있다
+		
+		//원
+		Circle c01 = new Circle(10);
+		Circle c02 = new Circle(20);
+		Circle c03 = new Circle(30);
+		
+		//원 리스트에 원(주소) 넣기
+		cList.add(c01);
+		cList.add(c02);
+		cList.add(c03);
+		
+		//출력
+		for(int i=0; i<cList.size(); i++) {
+			System.out.println(cList.get(i).toString());
+			//System.out.println(((Circle)cList.get(i)).toString());
+		}
 		
 		
 	}
--- src/com/javaex/ex02/MyList.java
+++ src/com/javaex/ex02/MyList.java
@@ -8,15 +8,15 @@ public class MyList {
 
 	// 생성자
 	public MyList() {
-		oArr = new MyList[3]; // 상상 추가하면 늘어나면 삭제하면 줄어든다
+		oArr = new Object[3]; // 상상 추가하면 늘어나면 삭제하면 줄어든다
 		crtPos = 0;
 	}
 
 	// 메소드 gs
 
 	// 메소드 일반
-	public void add(MyList p) {
-		this.oArr[crtPos] = p;
+	public void add(Object p) {
+		this.oArr[crtPos] = p; //업캐스팅 자동-섞어쓰기
 		this.crtPos++;
 	}
 
