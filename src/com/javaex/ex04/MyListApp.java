9d0bd9166bff6765a5aea03c7c3be6f8cbeead5b
ahah2025 <adghty61>
2025-05-13 15:51:23 +0900

    2025-05-13

---
+++ src/com/javaex/ex04/MyListApp.java
@@ -0,0 +1,65 @@
+package com.javaex.ex04;
+
+import java.util.ArrayList;
+
+public class MyListApp {
+	
+	public static void main(String[] args) {
+		//*포인트는 포인트별로 관리, 원은 원별로 관리
+		
+		//포인트 관리////////////////////////////////
+		//포인트 리스트
+		
+		ArrayList<Point> pList = new ArrayList<Point>();
+		
+		//포인트
+		Point p01 = new Point(2,2);
+		Point p02 = new Point(3,3);
+		Point p03 = new Point(4,4);
+		
+		//포인트리스트에 포인트(주소)를 넣는다
+		pList.add(p01);
+		pList.add(p02);
+		pList.add(p03);
+		
+		Point p = pList.get(0);
+		System.out.println(p.toString());
+		
+		for(int i=0; i<pList.size(); i++) {
+			System.out.println(pList.get(i).toString());
+		}
+		System.out.println(pList.toString());
+		System.out.println("-----------------------------------");
+		//원 관리////////////////////////////////
+		//원 리스트
+		
+		ArrayList<Circle> cList = new ArrayList<Circle>();
+		
+		//원리스트 포인트주소를 추가하면 안된다
+		Circle c01= new Circle(10);
+		Circle c02= new Circle(20);
+		Circle c03= new Circle(30);
+		
+		cList.add(c01);
+		cList.add(c02);
+		cList.add(c03);
+		
+		Circle c = cList.get(0);
+		System.out.println(c.toString());
+		
+		for(int a=0; a<cList.size() ; a++) {
+			System.out.println(cList.get(0).toString());
+		}
+		System.out.println("------------------------");
+		cList.remove(1); //1번방을 지워라
+		
+		for(int a=0; a<cList.size() ; a++) {
+			System.out.println(cList.get(0).toString());
+		}
+		System.out.println("------------------------");
+		
+		System.out.println(cList.toString());
+		
+	}
+
+}
